9f8d56a027a8cbaa20d478b8e0d15d2d19599149
s180608 <s180608@rguktsklm.ac.in>
2023-06-14 14:45:08 +0530

    completed building header component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Homepage from '../src/pages/homepage/homepage.component';
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/homepage/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
     <div>
       <Router>
+        <Header />
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route path="/shop" component={ ShopPage } />
